a118d1b82ff75b802f9b4a372e63b559f4955eab
Muhammad Iktear <iktear500@gmail.com>
2024-07-17 18:15:42 +0600

    update

--- pet_adoption/settings.py
+++ pet_adoption/settings.py
@@ -135,8 +135,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'

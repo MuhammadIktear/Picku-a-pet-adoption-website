bd395a023d58044d98b0fe66c15717607e37db5f
Muhammad Iktear <iktear500@gmail.com>
2024-08-04 13:08:41 +0600

    update

--- pet/views.py
+++ pet/views.py
@@ -41,6 +41,16 @@ class PetViewSet(viewsets.ModelViewSet):
     filterset_class = PetFilter
     search_fields = ['species__slug', 'sex__slug', 'color__slug', 'breed__slug', 'size__slug', 'status__slug','name']
     permission_classes = [AllowAny]
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        else:
+            print(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
 
 
 class PetDetailView(generics.RetrieveAPIView):

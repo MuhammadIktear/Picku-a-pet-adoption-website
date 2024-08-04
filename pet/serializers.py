8d5d2a96da99e201b1bcf4fce40ef8d724f10ea8
Muhammad Iktear <iktear500@gmail.com>
2024-08-04 11:26:18 +0600

    update

--- pet/serializers.py
+++ pet/serializers.py
@@ -7,17 +7,20 @@ class ReviewSerializer(serializers.ModelSerializer):
         fields = ['id', 'pet', 'author', 'author_username','author_email','body', 'created_at']
 
 class PetSerializer(serializers.ModelSerializer):
-    species = serializers.PrimaryKeyRelatedField(queryset=models.Species.objects.all())
-    sex = serializers.PrimaryKeyRelatedField(queryset=models.Sex.objects.all())
-    breed = serializers.PrimaryKeyRelatedField(queryset=models.Breed.objects.all())
-    color = serializers.PrimaryKeyRelatedField(queryset=models.Color.objects.all())
-    size = serializers.PrimaryKeyRelatedField(queryset=models.Size.objects.all())
-    status = serializers.PrimaryKeyRelatedField(queryset=models.Status.objects.all())
+    sex = serializers.StringRelatedField(many=True)  
+    species = serializers.StringRelatedField(many=True)  
+    color = serializers.StringRelatedField(many=True)  
+    breed = serializers.StringRelatedField(many=True)  
+    size = serializers.StringRelatedField(many=True)  
+    status = serializers.StringRelatedField(many=False)
     reviews = ReviewSerializer(many=True, read_only=True)
 
     class Meta:
         model = models.Pet
         fields = ['id', 'name', 'species', 'breed', 'color', 'size', 'sex', 'status', 'image', 'created_by', 'adopted_by', 'created_at', 'rehoming_fee', 'details', 'reviews']
+
+
+
 class SexSerializer(serializers.ModelSerializer):    
     class Meta:
         model = models.Sex

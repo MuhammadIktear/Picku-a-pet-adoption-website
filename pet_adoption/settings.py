3b2594abbeee025f7c75472f840dae8143b537d0
Muhammad Iktear <iktear500@gmail.com>
2024-08-08 00:18:22 +0600

    update

--- pet_adoption/settings.py
+++ pet_adoption/settings.py
@@ -39,6 +39,7 @@ CSRF_TRUSTED_ORIGINS = [
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "https://main--stirring-kitten-6cd5ff.netlify.app",
     "https://pet-adopt-website-picku.onrender.com",
 ]
 

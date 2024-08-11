ff6cfc58a5d430fc59f2845f1e4a33e2cea0f88d
Muhammad Iktear <iktear500@gmail.com>
2024-08-11 16:50:21 +0600

    update

--- user/models.py
+++ user/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.URLField(max_length=200,blank=True, null=True)
-    mobile_no = models.CharField(max_length=12)
+    mobile_no = models.CharField(max_length=15,blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
     twitter = models.URLField(max_length=200, blank=True, null=True)
     linkedin = models.URLField(max_length=200, blank=True, null=True)

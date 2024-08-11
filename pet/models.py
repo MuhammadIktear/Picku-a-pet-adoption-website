26581fee15c895916e7947c5796cde591edd760c
Muhammad Iktear <iktear500@gmail.com>
2024-08-11 14:45:06 +0600

    update

--- pet/models.py
+++ pet/models.py
@@ -45,7 +45,7 @@ class Pet(models.Model):
     size = models.ManyToManyField(Size)
     sex = models.ManyToManyField(Sex)
     status = models.ForeignKey(Status, on_delete=models.PROTECT)
-    image = models.FileField(upload_to='picku/pet_image', blank=True, null=True)
+    image = models.URLField(max_length=200,blank=True, null=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_pets')
     adopted_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='adopted_pets')
     created_at = models.DateTimeField(auto_now_add=True)

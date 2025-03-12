240321d2d39994643e4c98513d52819f19cd9196
Muhammad Iktear <iktear500@gmail.com>
2025-03-12 17:40:27 +0600

    database update

--- pet_adoption/settings.py
+++ pet_adoption/settings.py
@@ -30,11 +30,13 @@ ALLOWED_HOSTS = [
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://picku-a-pet-adoption-website.netlify.app",
+    "https://picku-a-pet-adoption-website.onrender.com",
 ]
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "https://picku-a-pet-adoption-website.netlify.app",
+    "https://picku-a-pet-adoption-website.onrender.com",
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
